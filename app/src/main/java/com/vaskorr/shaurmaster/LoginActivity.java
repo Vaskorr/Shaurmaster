3b0cf9974be90c71fc14cf28e52389ab237ee019
Vaskorr <basil.suntsov@yandex.ru>
2023-10-16 22:20:17 +0300

    add log-in

--- app/src/main/java/com/vaskorr/shaurmaster/LoginActivity.java
+++ app/src/main/java/com/vaskorr/shaurmaster/LoginActivity.java
@@ -18,55 +18,51 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.Objects;
-
-public class RegisterActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        Button registerButton = findViewById(R.id.login_button);
-        TextView loginRedirect = findViewById(R.id.register_redirect);
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_login);
+        Button login = findViewById(R.id.login_button);
+        TextView registerRedirect = findViewById(R.id.register_redirect);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                register();
+                login();
             }
         });
-        loginRedirect.setOnClickListener(new View.OnClickListener() {
+        registerRedirect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
         mAuth = FirebaseAuth.getInstance();
     }
 
-    private void register(){
-        String name = ((EditText) findViewById(R.id.name)).getText().toString();
+    private void login(){
         String email = ((EditText) findViewById(R.id.email)).getText().toString();
         String password = ((EditText) findViewById(R.id.password)).getText().toString();
-        String passwordCheck = ((EditText) findViewById(R.id.password_check)).getText().toString();
-        if (validate(name, email, password, passwordCheck)){
+        if (validate(email, password)){
             // If email and pass are correct, register new account
-            mAuth.createUserWithEmailAndPassword(email, password)
+            mAuth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 // Sign in success, update UI with the signed-in user's information
-                                Log.d("Register", "createUserWithEmail:success");
-                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                Log.d("Login", "signInWithEmailAndPassword:success");
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                                 startActivity(intent);
                                 setContentView(R.layout.activity_main);
                             } else {
                                 // If sign in fails, display a message to the user.
-                                Log.w("Register", "createUserWithEmail:failure", task.getException());
-                                Toast.makeText(RegisterActivity.this, "Не получилось. Попробуйте позже",
+                                Log.w("Login", "signInWithEmailAndPassword:failure", task.getException());
+                                Toast.makeText(LoginActivity.this, "Не получилось. Попробуйте позже",
                                         Toast.LENGTH_SHORT).show();
                             }
                         }
@@ -74,10 +70,10 @@ public class RegisterActivity extends AppCompatActivity {
         }
     }
 
-    private boolean validate(String name, String email, String password, String passwordCheck){
+    private boolean validate(String email, String password){
         TextView errorText = findViewById(R.id.error_text);
-        if (!Objects.equals(password, passwordCheck) || password.equals("")){
-            errorText.setText("Пароли не совпадают");
+        if (password.equals("")){
+            errorText.setText("Пароль не может быть пустым");
             return false;
         }
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
