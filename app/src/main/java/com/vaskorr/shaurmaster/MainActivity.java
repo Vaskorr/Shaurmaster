a436e8bb5b8cdb014c2ea9df78b1ff5326e6b977
Vaskorr <basil.suntsov@yandex.ru>
2023-10-16 20:02:36 +0300

    add Firebase

--- app/src/main/java/com/vaskorr/shaurmaster/MainActivity.java
+++ app/src/main/java/com/vaskorr/shaurmaster/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseAnalytics.getInstance(this);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
     }
 }
